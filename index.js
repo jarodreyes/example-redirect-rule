de7213a334313deac47ce5a8f21faed7e3210f39
Jarod Reyes <jarodreyes@gmail.com>
2020-11-12 10:13:37 -0800

    Added parent prompt logic.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.urlencoded({
@@ -11,17 +10,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/my-custom-prompt', (req, res) => {
+  .get('/parent-prompt', (req, res) => {
     const state = req.query.state;
 
     res.render('pages/color', { state });
   })
-  .post('/my-custom-prompt', (req, res) => {
+  .post('/parent-prompt', (req, res) => {
     console.log(req.body)
-    const { state, color } = req.body;
+    const { state, person } = req.body;
 
     console.log("Redirecting with state ", state);
 
-    res.redirect(`https://seejee-node-12-us.auth0.com/continue?state=${state}&color=${color}`);
+    res.redirect(`https://devrel.auth0.com/continue?state=${state}&person=${person}`);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
